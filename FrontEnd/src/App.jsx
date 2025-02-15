465dc7bb0ac0714c37421932be0adbc4805b156c
Chirag2109 <mittalchirag010@gmail.com>
2025-02-15 18:30:27 +0530

    added the feature to track plank time

---
+++ FrontEnd/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import PlankTracker from "./components/PlankTracker";
+
+const App = () => {
+    return (
+        <div>
+            <PlankTracker />
+        </div>
+    );
+};
+
+export default App;
