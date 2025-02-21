4a3056a69b50c549dcde10857a14af7e6a2f74f2
Chirag2109 <mittalchirag010@gmail.com>
2025-02-21 15:35:49 +0530

    changed some of the routes

--- FrontEnd/src/routes/index.jsx
+++ FrontEnd/src/routes/index.jsx
@@ -5,15 +5,17 @@ import PlankTracker from "../components/PlankTracker";
 import ForgotPassword from "../components/ForgotPassword";
 import PushupTracker from "../components/PushUpTracker";
 import SquatTracker from "../components/SquatsTracker";
+import LeaderBoard from "../components/LeaderBoard";
 
 const routes = [
   { path: "/", element: <Home /> },
-  { path: "/plank-tracker", element: <PlankTracker /> },
-  { path: "/pushup-tracker", element: <PushupTracker /> },
-  { path: "/squat-tracker", element: <SquatTracker /> },
-  { path: "/workout-tracker", element: <WorkoutTracker /> },
+  { path: "/leaderboard", element: <LeaderBoard /> },
+  { path: "/planktracker", element: <PlankTracker /> },
+  { path: "/pushuptracker", element: <PushupTracker /> },
+  { path: "/squattracker", element: <SquatTracker /> },
+  { path: "/workouttracker", element: <WorkoutTracker /> },
   { path: "/auth", element: <AuthPopup /> },
-  { path: "/forgot-password", element: <ForgotPassword /> },
+  { path: "/forgotpassword", element: <ForgotPassword /> },
 ];
 
 export default routes;
